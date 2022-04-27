aab7586d9245cf5363044439af4b46acd89209b7
Brandon Fahrenfort <mangacreeperyt@gmail.com>
2022-04-27 11:55:31 -0500

    Add initialization

--- src/Part1.cpp
+++ src/Part1.cpp
@@ -18,14 +18,63 @@
     #include <GL/glu.h>
 #endif // XCODE_SET_IN_GLOBALS
 
+#include <vector>
+
 int p1_window_id = -1;
+
+bool acquired_points = false;
+
+// Transformation utilities
+std::vector<Vec2<GLint>> polygon_points{ };
+Vec2<GLfloat> cur_translate, cur_scale{ };
+GLfloat rotTheta{ };
+
+// Color
+static GLfloat red{ }, green{ }, blue{ };
+
+void party()
+{
+    red = randf();
+    green = randf();
+    blue = randf();
+}
+
+void init()
+{
+    // TODO: acquire polygon points
+
+    glClearColor (1.0, 1.0, 1.0, 0.0);
+    party();
+}
+
+void p1_display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    if (acquired_points)
+    {
+        glPushMatrix();
+        glColor3f(red, green, blue);
+        glTranslatef(cur_translate.x, cur_translate.y, 1.0);
+        glRotatef(rotTheta, 0, 0, 1.0);
+        glScalef(cur_scale.x, cur_scale.y, 0);
+
+        glBegin(GL_POLYGON);
+        for(Vec2<GLint> p : polygon_points)
+            glVertex2i(p.x, p.y);
+        glEnd();
+
+        glPopMatrix();
+    }
+}
 
 
 void Part1::runPart1()
 {
     initrand();
-    glutCreateWindow("Part 1");
-    //glutDisplayFunc (displayHex);
+    init();
+    p1_window_id = glutCreateWindow("Part 1");
+    glutDisplayFunc(p1_display);
     //glutReshapeFunc (winReshapeFcn);
     //glutMouseFunc (mouseFcn);
 }
