459ec05072280afb9fbf9a7a1dcb7869371d0219
daghancetin <67640092+daghancetin@users.noreply.github.com>
2022-04-27 00:07:25 -0500

    Update Part1.cpp

--- src/Part1.cpp
+++ src/Part1.cpp
@@ -4,17 +4,21 @@
 
 #ifdef __APPLE__
     #include <GLUT/glut.h>
+
 #else
     #include <GL/glut.h>
+
 #endif // __APPLE__
 
 #ifdef XCODE_SET_IN_GLOBALS
     #define GL_SILENCE_DEPRECATION
     #include <OpenGL/gl.h>
     #include <OpenGL/glu.h>
+
 #else
     #include <GL/gl.h>
     #include <GL/glu.h>
+
 #endif // XCODE_SET_IN_GLOBALS
 
 int p1_window_id = -1;
